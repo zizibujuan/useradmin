acc7c1414ac323ed1df3b153489ef5d1d31f5679
xiaohulu <idocument@qq.com>
2014-09-21 17:10:59 +0800

    如果根据local token找不到用户，则清除用户登录的cookie

--- server/bundles/com.zizibujuan.useradmin.server.configurator/src/com/zizibujuan/useradmin/server/configurator/AutoLoginFilter.java
+++ server/bundles/com.zizibujuan.useradmin.server.configurator/src/com/zizibujuan/useradmin/server/configurator/AutoLoginFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,9 +44,10 @@ public class AutoLoginFilter implements Filter{
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
 			FilterChain filterChain) throws IOException, ServletException {
 		final HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+		final HttpServletResponse httpResponse = (HttpServletResponse)servletResponse;
 		
 		if(!UserSession.isLogged(httpRequest)){
-			tryAutoLogin(httpRequest);
+			tryAutoLogin(httpRequest, httpResponse);
 		}
 		filterChain.doFilter(servletRequest, servletResponse);
 		
@@ -56,7 +58,7 @@ public class AutoLoginFilter implements Filter{
 		userService = UserAdminServiceHolder.getDefault().getUserService();
 	}
 	
-	private void tryAutoLogin(final HttpServletRequest httpRequest) {
+	private void tryAutoLogin(final HttpServletRequest httpRequest, final HttpServletResponse httpResponse) {
 		// 如果cookie中有logged_in和token，则执行登录操作
 		Cookie loggedInCookie = CookieUtil.get(httpRequest, CookieConstants.LOGGED_IN);
 		// TODO:用户每次登录，都自动分配一个token
@@ -68,6 +70,11 @@ public class AutoLoginFilter implements Filter{
 				UserInfo userInfo = userService.getByToken(localAccessToken);
 				if(userInfo == null){
 					logger.error("自动登录失败，这个时候页面上会显示LoggedInHeader，出现不一致的情况");
+					// 如果用户在两台电脑上登录，那么就会出现在另一台电脑上缓存的token发生变化，
+					// 从而根据token找不到用户的问题，这时要清除当前电脑的cookie
+					CookieUtil.remove(httpRequest, httpResponse, CookieConstants.LOGGED_IN);
+					CookieUtil.remove(httpRequest, httpResponse, CookieConstants.LOGIN_NAME);
+					CookieUtil.remove(httpRequest, httpResponse, CookieConstants.ZZBJ_USER_TOKEN);
 				}else{
 					UserSession.setUser(httpRequest, userInfo);
 				}
--- server/bundles/com.zizibujuan.useradmin.server.servlets/src/com/zizibujuan/useradmin/server/servlets/connect/UserConnect.java
+++ server/bundles/com.zizibujuan.useradmin.server.servlets/src/com/zizibujuan/useradmin/server/servlets/connect/UserConnect.java
@@ -87,8 +87,6 @@ public abstract class UserConnect {
 		String localAccessToken = IdGenerator.uuid();
 		com.zizibujuan.useradmin.server.model.UserInfo dripUserInfo = userService.login(dripUserId, localAccessToken, oauthAccessToken, oauthTokenExpireIn);
 		UserSession.setUser(req, dripUserInfo);
-		// 第三方网站不在loginName中缓存昵称
-		CookieUtil.set(resp, CookieConstants.LOGGED_IN, "1", null, -1);
 		// 防止同一台电脑先使用drip用户登录，然后使用qq用户登录，要删除本网站的token
 		// 这样就不会使用其他人的帐号自动登录
 		// 同时也可以设置第三方网站的token，这样可以使用这些token自动登录
@@ -96,6 +94,8 @@ public abstract class UserConnect {
 		CookieUtil.set(resp, CookieConstants.LOGIN_NAME, loginName, null, 365*24*60*60/*一年有效*/);
 		CookieUtil.set(resp, CookieConstants.LOGGED_IN, "1", null, -1);
 		CookieUtil.set(resp, CookieConstants.ZZBJ_USER_TOKEN, localAccessToken, null, -1);
+		
+		
 		if(dripUserInfo.getEmail() == null || dripUserInfo.getEmail().trim().isEmpty()){
 			return true;
 		}
